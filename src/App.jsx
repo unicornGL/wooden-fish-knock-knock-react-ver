ecfda946ce6afcddcfb1d06f542f8d7f61dad0d8
unicornGL <s941407@gmail.com>
2024-07-21 02:32:41 +0800

    [feat] set css reset and update dir structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Layout from "./Layout"
+import CssBaseline from "@mui/material/CssBaseline"
+// TODO: set into index.scss
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import Home from "./home/Home"
 
 const App = () => {
   return (
     <>
-      <Layout />
+      <CssBaseline />
+      <Home />
     </>
   )
 }
